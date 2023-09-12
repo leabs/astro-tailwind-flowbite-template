a1a622839ea195719e0ec63e11c1e7d1290bbd80
Steve <stevenleabo@gmail.com>
2023-09-12 11:24:00 -0400

    Moving more stuff into data/site.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
+const owner = "astrojs";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()]
