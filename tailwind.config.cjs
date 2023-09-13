b2230f6069f3195dc85eeb91f411a4bbc22be8ca
Steve <stevenleabo@gmail.com>
2023-09-13 09:44:37 -0400

    darkmode with local storage working

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+	darkMode: 'class',
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			translate: ['dark'],
+		},
 		
 	},
 	plugins: [],
