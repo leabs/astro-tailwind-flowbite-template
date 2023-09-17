e4f5526d2db29be6bb8d8065846296e4df56c68c
Leabs <stevenleabo@gmail.com>
2023-09-17 18:35:24 -0400

    updating layouts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	darkMode: 'class',
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			translate: ['dark'],
-		},
-		
-	},
-	plugins: [],
-}
+  darkMode: "class",
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    fill: ({ theme }) => ({
+      indigo: theme("colors.blue"),
+    }),
+    extend: {
+      translate: ["dark"],
+    },
+  },
+};
