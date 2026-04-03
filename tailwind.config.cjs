2cb790bb4db67930999a2d645ff89bdea6408e1a
Steven Leabo <stevenleabo@gmail.com>
2026-04-03 13:58:32 -0400

    updating node v, tailwind and classes, other packages

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,44 +3,45 @@ const colors = require("tailwindcss/colors");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
-    "./node_modules/flowbite/**/*.js",
-  ],
   theme: {
     colors: {
-      primary: "#17202A",
-      secondary: "#ecc94b",
+      primary: {
+        ...colors.slate,
+        DEFAULT: "#17202A",
+      },
+      secondary: {
+        ...colors.amber,
+        DEFAULT: "#ecc94b",
+      },
       transparent: "transparent",
       current: "currentColor",
+      black: colors.black,
+      white: colors.white,
       slate: colors.slate,
       stone: colors.stone,
       blue: colors.blue,
       green: colors.green,
-      black: colors.black,
-      white: colors.white,
       gray: colors.gray,
       emerald: colors.emerald,
       indigo: colors.indigo,
       yellow: colors.yellow,
     },
     extend: {
-      translate: ["dark"],
       animation: {
-        'skew-scroll': 'skew-scroll 20s linear infinite',
+        "skew-scroll": "skew-scroll 20s linear infinite",
       },
-    },
       keyframes: {
-        'skew-scroll': {
-          '0%': {
-            transform: 'rotatex(20deg) rotateZ(-20deg) skewX(20deg) translateZ(0) translateY(0)',
-          },
-          '100%': {
+        "skew-scroll": {
+          "0%": {
             transform:
-              'rotatex(20deg) rotateZ(-20deg) skewX(20deg) translateZ(0) translateY(-100%)',
+              "rotatex(20deg) rotateZ(-20deg) skewX(20deg) translateZ(0) translateY(0)",
+          },
+          "100%": {
+            transform:
+              "rotatex(20deg) rotateZ(-20deg) skewX(20deg) translateZ(0) translateY(-100%)",
           },
         },
       },
+    },
   },
-  plugins: [require("flowbite/plugin")],
 };
